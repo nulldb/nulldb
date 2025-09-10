475cc4ba69313aa8a06e590038b9fbce56fac650
Evan Brooks <evaniainbrooks@gmail.com>
2025-09-10 10:02:45 -0300

    add exclusion_constraint to table_definition

--- lib/active_record/connection_adapters/nulldb_adapter/table_definition.rb
+++ lib/active_record/connection_adapters/nulldb_adapter/table_definition.rb
@@ -1,19 +1,19 @@
 class ActiveRecord::ConnectionAdapters::NullDBAdapter
-
   class TableDefinition < ActiveRecord::ConnectionAdapters::TableDefinition
     attr_accessor :name
-    alias_method :enum, :string
-    alias_method :uuid, :string
-    alias_method :citext, :text
-    alias_method :interval, :text
-    alias_method :geometry, :text
-    alias_method :serial, :integer
-    alias_method :bigserial, :integer
-    alias_method :inet, :string
-    alias_method :jsonb, :json if method_defined? :json
-    alias_method :hstore, :json
+    alias enum string
+    alias uuid string
+    alias citext text
+    alias interval text
+    alias geometry text
+    alias serial integer
+    alias bigserial integer
+    alias inet string
+    alias jsonb json if method_defined? :json
+    alias hstore json
 
     def unique_constraint(*args, **kwargs, &block); end
+    def exclusion_constraint(*args); end
 
     if ::ActiveRecord::VERSION::MAJOR >= 7 && ::ActiveRecord::VERSION::MINOR >= 1
       # Avoid check for option validity
